7a9897352a5f627443589e2994d6b6db401045de
Fereydoon <epfereydoon@gmail.com>
2022-08-16 17:46:06 +0000

    order and trade update in TradingCoin.js

--- src/pages/TradingCoin.js
+++ src/pages/TradingCoin.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import CoinPriceLive from "../components/CoinPriceLive";
-
+import TradingChart from "./../components/TradingChart";
 const TradingCoin = () => {
-  const { coin } = useParams();
+  // const { coin } = useParams();
+  const coin = "eth";
   const [BTC, setBTC] = useState(Number); //  !  BTC PRICE
 
   const [usdtWallet, setUsdtWallet] = useState(100000); //! USDST Wallet
@@ -41,108 +42,142 @@ const TradingCoin = () => {
     if (btcBuy === 0) return;
     const newOrder = {
       id: Date.now(),
-      ordersCoinName: "BTC",
+      ordersCoinName: coin.toUpperCase(),
       orderCoinAmount: btcInput,
       orderCointPrice: usdtInput,
     };
     setOrders([...orders, newOrder]);
+    // todo  Your recent orders
+    // Side	Order price	the amount of	currency	Total Amount	full	Actions
+    // Buy	17,000	0.045	BTC	765 Tether	0%	Cancel the order
   };
-
   const priceRef = useRef();
+  //const [orderMatch, setOrderMatch] = useState([]);
+
   useEffect(() => {
     if (BTC && orders.length > 0) {
       setInterval(() => {
         const priceNow = priceRef.current.innerText;
-        if (priceNow === orders[0].orderCointPrice) {
-          confirmOrder();
-        }
-      }, 1000);
-    }
-  }, [BTC, orders]);
+        // if (priceNow === orders[0].orderCointPrice) {
+        //   confirmOrder();
+        // }
+        const orderMatchFind = orders.find((item) => item.orderCointPrice === priceNow);
+        //if (orderMatchFind === true)
+        if (orderMatchFind !== undefined) {
+          //  setOrderMatch(orderMatchFind);
+          setOrders(orders.filter((item) => item.id !== orderMatchFind.id));
 
-  const confirmOrder = () => {
-    const newTrade = {
-      id: Date.now(),
-      tradeCoinName: "BTC",
-      tradeCoinAmount: orders[0].orderCoinAmount,
-    };
-    setTrades([...trades, newTrade]);
-    setUsdtWallet(usdtWallet - orders[0].orderCointPrice);
-    setOrders(orders.push(orders[0]));
-  };
+          ///////////////////////////////
+          const newTrade = {
+            id: Date.now(),
+            tradeCoinName: coin.toUpperCase(),
+            tradeCoinAmount: orderMatchFind.orderCoinAmount,
+            tradeCoinPrice: orderMatchFind.orderCointPrice,
+          };
+          setTrades([...trades, newTrade]);
+          setUsdtWallet(usdtWallet - orderMatchFind.orderCointPrice);
+        }
+      }, 10);
+    }
+  }, [BTC, orders, trades, usdtWallet]);
+
   return (
     <>
-      <div ref={priceRef}>
-        {/* {BTC} */}
-        <CoinPriceLive symbol={coin} />
-      </div>
-      {/* <TradingChart /> */}
-      <div className="w-50 m-3 p-3 border border-1">
-        <div className="pb-3 text-center text-dark">Buy {coin.toLocaleUpperCase()}</div>
-        <div className="">
-          <div className="input-group mb-3 px-4">
-            <span
-              className="input-group-text"
-              style={{ padding: "8px  44px" }}
-              id="inputGroup-sizing-default"
-            >
-              USDT
-            </span>
-            <input
-              value={usdtInput}
-              onChange={(event) => usdtInputHandler(event)}
-              type="number"
-              className="form-control"
-            />
-          </div>
+      <div className="d-flex row">
+        <div className="trade d-flex">
+          <div className="w-50 m-3 p-3 border border-1">
+            <div className="pb-3 text-center text-dark">Buy {coin.toLocaleUpperCase()}</div>
+            <div className="">
+              <div className="input-group mb-3 px-4">
+                <span
+                  className="input-group-text"
+                  style={{ padding: "8px  44px" }}
+                  id="inputGroup-sizing-default"
+                >
+                  USDT
+                </span>
+                <input
+                  value={usdtInput}
+                  onChange={(event) => usdtInputHandler(event)}
+                  type="number"
+                  className="form-control"
+                />
+              </div>
 
-          <div className="input-group mb-3 px-4">
-            <span
-              className="input-group-text  "
-              style={{ padding: "8px  50px" }}
-              id="inputGroup-sizing-default"
-            >
-              {coin.toUpperCase()}
-            </span>
-            <input
-              value={btcInput}
-              onChange={(event) => btcInputHandler(event)}
-              type="number"
-              className="form-control"
-              aria-label="Sizing example input"
-              aria-describedby="inputGroup-sizing-default"
-            />
+              <div className="input-group mb-3 px-4">
+                <span
+                  className="input-group-text  "
+                  style={{ padding: "8px  50px" }}
+                  id="inputGroup-sizing-default"
+                >
+                  {coin.toUpperCase()}
+                </span>
+                <input
+                  value={btcInput}
+                  onChange={(event) => btcInputHandler(event)}
+                  type="number"
+                  className="form-control"
+                  aria-label="Sizing example input"
+                  aria-describedby="inputGroup-sizing-default"
+                />
+              </div>
+              <div className="input-group   mb-3 px-4 d-flex">
+                <span className="input-group-text w-75 border border-0 ">USDT in WALLET</span>
+                <span className="input-group-text w-25 border border-0 ">{usdtWallet}</span>
+              </div>
+            </div>
+            <div className="">You recived : {btcBuy}</div>
+            <div className="  mb-3 px-4">
+              <button
+                className="btn  btn-primary w-100"
+                disabled={btcBuy > usdtWallet}
+                onClick={(e) => setOrderHandler(e)}
+              >
+                Buy
+              </button>
+              <span>{btcBuy > usdtWallet ? "Please Deposit USDT" : ""}</span>
+            </div>
+            <h1>Orders</h1>
+            {orders.length
+              ? orders.map((item) => (
+                  <div
+                    className="d-flex justify-content-between border border-1 p-1 m-3"
+                    key={item.id}
+                  >
+                    <div className="">{item.ordersCoinName}</div>
+                    <div className="">{item.orderCoinAmount}</div>
+                    <div className="">{item.orderCointPrice}</div>
+                  </div>
+                ))
+              : null}
+            <hr />
+            <h1>Trades</h1>
+            {trades.length > 0
+              ? trades.map((item) => (
+                  <div
+                    className="d-flex justify-content-between border border-1 p-1 m-3"
+                    key={item.id}
+                  >
+                    <div className=""> You Buy </div>
+                    <div className=""> : {item.tradeCoinAmount}</div>
+                    <div className=""> {item.tradeCoinName}</div>
+                    <div className=""> in {item.tradeCoinPrice} USDT</div>
+                  </div>
+                ))
+              : null}
           </div>
-          <div className="input-group   mb-3 px-4 d-flex">
-            <span className="input-group-text w-75 border border-0 ">USDT in WALLET</span>
-            <span className="input-group-text w-25 border border-0 ">{usdtWallet}</span>
-          </div>
-        </div>
-        <div className="">You recived : {btcBuy}</div>
-        <div className="  mb-3 px-4">
-          <button
-            className="btn  btn-primary w-100"
-            disabled={btcBuy > usdtWallet}
-            onClick={(e) => setOrderHandler(e)}
+          <div
+            className="display-3 d-flex justify-content-center align-items-center w-50"
+            ref={priceRef}
           >
-            Buy
-          </button>
-          <span>{btcBuy > usdtWallet ? "Please Deposit USDT" : ""}</span>
+            {/* {BTC} */}
+            <CoinPriceLive symbol={coin} />
+          </div>
         </div>
-        {orders.map((item) => (
-          <div className="d-flex justify-content-between border border-1 p-1 m-3" key={item.id}>
-            <div className="">{item.ordersCoinName}</div>
-            <div className="">{item.orderCoinAmount}</div>
-            <div className="">{item.orderCointPrice}</div>
-          </div>
-        ))}
-
-        {trades.map((item) => (
-          <div className="d-flex justify-content-between border border-1 p-1 m-3" key={item.id}>
-            <div className="">you buy : {item.tradeCoinName}</div>
-            <div className=""> : {item.tradeCoinAmount}</div>
-          </div>
-        ))}
+        <div className="chart  justify-content-center">
+          <div className="w-75"></div>
+            <TradingChart />
+        </div>
       </div>
     </>
   );
