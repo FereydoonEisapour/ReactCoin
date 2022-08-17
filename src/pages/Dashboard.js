9be806bc6bdde1ded184e3331d7542009f850576
Fereydoon <epfereydoon@gmail.com>
2022-08-17 18:53:55 +0000

    update dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "../contexts/AuthContext";
 import { Navigate } from "react-router-dom";
+import db from "./../data/Firebase";
+import firebase from "firebase/compat/app";
 const Dashboard = () => {
   const { user } = useAuthState();
+  const [coins, setCoins] = useState([]);
   if (!user) return <Navigate to="/" />;
-  return <div>Dashboard</div>;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    db.collection(user.email)
+      .doc(user.email)
+      .collection("coins")
+      .onSnapshot((snapshot) => {
+        setCoins(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            coin: doc.data().coin,
+            amount: doc.data().amount,
+          }))
+        );
+      });
+  }, [user.email]);
+
+  const addCoin = (e) => {
+    e.preventDefault();
+    db.collection(user.email).doc(user.email).collection("coins").add({
+      coin: "ADA",
+      amount: "100",
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+  };
+
+  return (
+    <div className="container d-flex  p-1  ">
+      <div className="col-12 d-flex row  p-3 mx-4">
+        {coins.map((item) => (
+          <div className="col-5 p-4 m-2 d-flex border justify-content-around bg-info text-light display-6" key={item.id}>
+            <div className="">{item.coin} </div>
+            <div className="">  {item.amount}</div>
+          </div>
+        ))}
+      </div>
+      {/* <div className="">
+        <button onClick={(e) => addCoin(e)}>add usdt</button>
+      </div> */}
+    </div>
+  );
 };
 
 export default Dashboard;
