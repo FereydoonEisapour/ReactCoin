de9ed403f6851073b1f93e1cfc37819c68cab774
Fereydoon <epfereydoon@gmail.com>
2022-08-20 12:00:23 +0000

    vercel firebase

--- src/data/Firebase.js
+++ src/data/Firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase/compat/app";
+import firebase from "firebase/compat";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 const firebaseApp = firebase.initializeApp({
