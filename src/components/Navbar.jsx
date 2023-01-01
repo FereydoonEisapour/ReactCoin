5155b5bc878e19152eceea8fb19862de982b98a9
fereydoon <epfereydoon@gmail.com>
2023-01-01 23:29:25 +0330

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,40 +12,45 @@ const Navbar = () => {
           >
             ReactCoin
           </Link>
-       
-            <>
-              <Link
-                className="text-decoration-none  px-1 text-dark fw-bold "
-                to="/trade/btc"
-              >
-                Trade
-              </Link>
-              <Link
-                className="text-decoration-none px-1 text-dark fw-bold"
-                to="/buyfast/btc"
-              >
-                Buy Now
-              </Link>
-            </>
-       
-       
+
+          <>
+            <Link
+              className="text-decoration-none  px-1 text-dark fw-bold "
+              to="/trade/btc"
+            >
+              Trade
+            </Link>
+            <Link
+              className="text-decoration-none px-1 text-dark fw-bold"
+              to="/buyfast/btc"
+            >
+              Buy Now
+            </Link>
+          </>
         </nav>
       </div>
       <div className="d-flex align-items-center px-1">
-        <ul className="navbar-nav me-auto mb-2  my-2">
-     
-            <Link
-              to="/dashboard"
-              className="nav-item mx-1 text-decoration-none text-dark fw-bold"
-            >
-              Dashboard
-            </Link>
-    
-            <></>
-       
-        </ul>
+        {/* <ul className="navbar-nav me-auto mb-2  my-2">
+          <Link
+            to="/dashboard"
+            className="nav-item mx-1 text-decoration-none text-dark fw-bold"
+          >
+            Dashboard
+          </Link>
+
+          <></>
+        </ul> */}
         <div className="d-flex">
-          {user ? null : (
+          {user ? (
+            <div className="me-auto mb-2  my-2">
+              <Link
+                to="/dashboard"
+                className="nav-item mx-1 text-decoration-none text-dark fw-bold"
+              >
+                Dashboard
+              </Link>
+            </div>
+          ) : (
             <Link
               to="/login"
               className="nav-item mx-1 text-decoration-none text-dark fw-bold"
