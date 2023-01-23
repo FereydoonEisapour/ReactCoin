f4ae26c965cca181d0b335ec7186d3c899015ab3
fereydoon <epfereydoon@gmail.com>
2023-01-23 10:14:11 -0800

    use tradingview to trading chart

--- src/components/TradingChart.jsx
+++ src/components/TradingChart.jsx
@@ -1,28 +1,59 @@
-import React from "react";
+
+import React, { useEffect, useRef } from 'react';
 import { useParams } from "react-router-dom";
-import TradingViewWidget, { Themes, IntervalTypes } from "react-tradingview-widget";
+//import useDarkMode from '../hooks/useDarkMode'
+let tvScriptLoadingPromise;
 
-const TradingChart = ({ height, width }) => {
+export default function TradingViewWidget() {
+  const onLoadScriptRef = useRef();
   const { coin } = useParams();
-  const [symbol, setSymbol] = React.useState("BTCUSDT");
-  const tradingRef = React.useRef(null);
+ // const [ theme] = useDarkMode()
+  
+  useEffect(
+    () => {
+      onLoadScriptRef.current = createWidget;
 
-  React.useEffect(() => {
-    //  console.log(tradingRef.current.props.symbol);
-    setSymbol(`${coin.toUpperCase()}USDT`);
-  }, [coin]);
+      if (!tvScriptLoadingPromise) {
+        tvScriptLoadingPromise = new Promise((resolve) => {
+          const script = document.createElement('script');
+          script.id = 'tradingview-widget-loading-script';
+          script.src = 'https://s3.tradingview.com/tv.js';
+          script.type = 'text/javascript';
+          script.onload = resolve;
+
+          document.head.appendChild(script);
+        });
+      }
+
+      tvScriptLoadingPromise.then(() => onLoadScriptRef.current && onLoadScriptRef.current());
+
+      return () => onLoadScriptRef.current = null;
+
+      function createWidget() {
+        if (document.getElementById('tradingview_12bac') && 'TradingView' in window) {
+          new window.TradingView.widget({
+            width: "100%",
+            height: "100%",
+            symbol:  `BINANCE:${coin}USDT`,
+            interval: "240",
+            timezone: "Etc/UTC",
+          //  theme: theme,
+            style: "1",
+            locale: "en",
+            toolbar_bg: "#f1f3f6",
+            enable_publishing: false,
+            allow_symbol_change: true,
+            container_id: "tradingview_12bac",
+          });
+        }
+      }
+    },
+    [coin,]
+  );
 
   return (
-    <TradingViewWidget
-      height={height}
-      width={width}
-      symbol={symbol}
-      theme={Themes.LIGHT}
-      locale="en"
-      ref={tradingRef}
-      IntervalTypes={IntervalTypes.D}
-      autosize
-    />
+    <div className='tradingview-widget-container  '>
+      <div id='tradingview_12bac' className=''/>
+    </div>
   );
-};
-export default TradingChart;
+}
