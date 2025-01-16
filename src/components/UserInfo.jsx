242f6490f67129c2348e0b0f32d164b54af1e8ce
Fereydoon <epfereydoon@gmail.com>
2025-01-16 21:46:58 +0330

    Update UserInfo.jsx

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -4,31 +4,23 @@ import {
   useAuthDispatch,
   useAuthState,
 } from "../contexts/AuthContext";
-
-
 function UserInfo() {
   const { userEmail } = useAuthState();
   const dispatch = useAuthDispatch();
-
   const logOut = () => {
     doLogOut(dispatch);
   };
-
   return (
-    <div className="user-info-container card shadow-sm p-4">
-      <div className="avatar-section text-center mb-4">
-        <div className="avatar rounded-circle bg-primary text-white d-inline-flex align-items-center justify-content-center">
-          <span style={{ fontSize: "2rem" }}>😊</span>
-        </div>
-        <h3 className="mt-3">Welcome!</h3>
+    <div>
+      <div className="">
+        <div className="avatar ">avatar : )</div>
       </div>
-      <div className="email-section text-center mb-4">
-        <p className="text-muted">Email:</p>
-        <h5>{userEmail}</h5>
+      <div className="mt-3">
+        <div className="">Email : {userEmail}</div>
       </div>
-      <div className="logout-section text-center">
-        <button className="btn btn-warning px-4 py-2" onClick={logOut}>
-          Log Out
+      <div className="mt-3 text-white  ">
+        <button className="btn col-4 btn-warning" onClick={logOut}>
+          Log out
         </button>
       </div>
     </div>
